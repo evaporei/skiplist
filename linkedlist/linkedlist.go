910283f2dbf620b3764b931b65e2d1fcfe42b707
Eva Pace <eba.pachi@gmail.com>
2024-03-29 09:11:55 -0300

    accessor for Value too (is this Java???)

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,25 +1,29 @@
 package linkedlist
 
 type LinkedList[T any] struct {
-    Value T
+    value T
     next *LinkedList[T]
 }
 
 func New[T any](value T) *LinkedList[T] {
     return &LinkedList[T] {
-        Value: value,
+        value: value,
         next: nil,
     }
 }
 
 func (l *LinkedList[T]) Push(value T) {
-    oldNode := New(l.Value)
-    l.Value = value
+    oldNode := New(l.value)
+    l.value = value
     l.next = oldNode
 }
 
 func (l *LinkedList[T]) Pop() {
     *l = *l.next
+}
+
+func (l *LinkedList[T]) Value() T {
+    return l.value
 }
 
 func (l *LinkedList[T]) Next() *LinkedList[T] {
