933775210333522ebcf244f8dc840425aea82ca2
Eva Pace <eba.pachi@gmail.com>
2024-03-29 09:10:29 -0300

    add comments in main

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
     l := linkedlist.New(2)
     l.Push(4)
-    fmt.Println("first", l)
-    fmt.Println("next", l.Next())
+    fmt.Println("first", l) // { value: 4, next: { value: 2, next: nil } }
+    fmt.Println("next", l.Next()) // { value: 2, next: nil }
 
     fmt.Println("Pop()")
     l.Pop()
-    fmt.Println("first", l)
-    fmt.Println("next", l.Next())
+    fmt.Println("first", l) // { value: 2, next: nil }
+    fmt.Println("next", l.Next()) // nil
 }
